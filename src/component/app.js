3dbe1c9c57bab989219e420addc6fd613414101e
billodell <billodell5@gmail.com>
2018-03-19 12:00:24 -0700

    front end scaffold

---
+++ src/component/app.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Landing from './landing/landing'
+import {Provider} from 'react-redux'
+import createStore from '../lib/app-create-store'
+import {BrowserRouter, Route, Redirect} from 'react-router-dom'
+const store = createStore()
+
+export default class App extends React.Component {
+//   componentWillMount() {
+//     if(localStorage.token)
+//       store.dispatch({type: 'TOKEN_SET', payload: localStorage.token})
+//   }
+
+
+  render() {
+    let {token} = store.getState()
+
+    return (
+      <main className="application">
+        <Provider store={store}>
+          <BrowserRouter>
+            <React.Fragment>
+              <Route exact path="/" component={Landing}/>
+            </React.Fragment>
+          </BrowserRouter>
+        </Provider>
+      </main>
+    )
+  }
+}
