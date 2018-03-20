b40c1f90ef2fb2470842a9e3acb0ec180f67c27e
montgomeryrd <montgomery_rd@yahoo.com>
2018-03-20 10:31:29 -0700

    removed modal code

--- src/component/landing/landing.js
+++ src/component/landing/landing.js
@@ -1,23 +1,16 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import ClipItem from '../clip/clip'
-import ModalButton from '../modal/modal-button';
-import Modal from '../modal/modal';
-
 
 class Landing extends React.Component {
   render() {
-    const {buttonContainer} = styles;
     
     return (
-      <Modal />
       <div className="landing-container">
         <h1>Twitch Clip Manager</h1>
         {this.props.clips ?
           this.props.clips.map(clip => <ClipItem key={clip.tracking_id} clip={clip}/>) :
           undefined}
-
-          <ModalButton style={buttonContainer}/>
       </div>
     )
   }
@@ -30,14 +23,5 @@ let mapStateToProps = state => ({
 //   signup: user => dispatch(signupRequest(user)),
 //   signin: user => dispatch(signinRequest(user)),
 // })
-
-const styles = {
-  buttonContainer: {
-    width: 600,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingBottom: 10
-  }
-}
 
 export default connect(mapStateToProps)(Landing);
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,8 +1,7 @@
 import {combineReducers} from 'redux';
 import clips from './clips';
-import modal from './modal-reducer';
 // import picture from './picture';
 
 export default combineReducers({
-    clips, modal
+    clips
 });
